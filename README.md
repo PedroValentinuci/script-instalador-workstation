0835348475a773807547d514f674ebf2464be17a
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-02 20:51:42 -0300

    readme atualizado e script criado

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # script-instalador-workstation
- instalador com minha workstation
+ instalador com minha workstation, se quiser melhorar algo pode viu meu conssagra!!!
---
+++ script/instalador.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Removendo travas do apt ##
+
+sudo rm /var/lib/dpkg/lock-fronted; sudo rm /var/cache/apt/archives/lock;
+
+## Atualizando repositório ##
+
+sudo apt update 
+
+## Downloads de arquivos especificos ##
+
+mkdir /home/SUSER/Downloads/programas
+
+cd /home/SUSER/Downloads/programas
+
+wget -c https://github.com/shiftkey/desktop/releases/download/release-2.4.1-linux2/GitHubDesktop-linux-2.4.1-linux2.deb
+wget -c https://discordapp.com/api/download?platform=linux&format=deb
+
+sudo dpkg -i *.deb -y
+
+## Downloads das linguagens ##
+
+sudo apt update
+sudo apt install git -y
+sudo apt install ruby -y
+sudo apt install rbenv -y
+sudo apt install python -y
+
+## Download des programas ##
+
+sudo apt install gimp -y
+sudo apt update
+
+echo "Prontinho meu conssagra"
