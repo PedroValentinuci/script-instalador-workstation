f98547ac818a09ef66a91cc06f8b8b3f1389e630
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-02 21:16:47 -0300

    agora sim tá compelto

--- script/instalador.sh
+++ script/instalador.sh
@@ -16,6 +16,7 @@ cd /home/SUSER/Downloads/programas
 
 wget -c https://github.com/shiftkey/desktop/releases/download/release-2.4.1-linux2/GitHubDesktop-linux-2.4.1-linux2.deb
 wget -c https://discordapp.com/api/download?platform=linux&format=deb
+wget -c google-chrome-stable_current_amd64.debgoogle-chrome-stable_current_amd64.deb
 
 sudo dpkg -i *.deb 
 
@@ -30,6 +31,8 @@ sudo apt install python -y
 ## Download des programas ##
 
 sudo apt install gimp -y
+sudo apt install libreoffice -y
+sudo apt install arduino -y
 sudo apt update
 
 echo "Prontinho meu conssagra"
