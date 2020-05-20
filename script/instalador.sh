b0d22e2d4f2446509f21fbc33bbe1cf2ce0b01f1
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-20 10:07:08 -0300

    instalador atualizado
    snapd
    freecad
    onlyoffice
    flutter

--- script/instalador.sh
+++ script/instalador.sh
@@ -18,6 +18,7 @@ wget -c https://github.com/shiftkey/desktop/releases/download/release-2.4.1-linu
 wget -c https://discordapp.com/api/download?platform=linux&format=deb
 wget -c google-chrome-stable_current_amd64.debgoogle-chrome-stable_current_amd64.deb
 wget -c https://github.com/KryDos/todoist-linux/releases/download/1.21/Todoist_1.21.0_amd64.deb
+wget -c https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_1.17.1-stable.tar.xz
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
 
 sudo dpkg -i *.deb 
@@ -29,19 +30,26 @@ sudo apt install git -y
 sudo apt install ruby -y
 sudo apt install rbenv -y
 sudo apt install python -y
+sudo apt install snapd -y
 
 ## Downloads dos programas apt ##
 
 sudo apt install gimp -y
 sudo apt install libreoffice -y
 sudo apt install arduino -y
-sudo apt install librecad -y
+sudo apt install freecad -y
 sudo apt update
 
 ## Dowloads dos programas snaps ##
 
 sudo snap install pycharm-community --classic
-sudo snap install librepcb
-sudo apt update
+sudo snap install onlyoffice-desktopeditors
+
+## Downloads complexos ##
+
+## Flutter ##
+cd ~/development
+tar xf ~/Downloads/flutter_linux_1.17.1-stable.tar.xz
+export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Prontinho meu conssagra"
